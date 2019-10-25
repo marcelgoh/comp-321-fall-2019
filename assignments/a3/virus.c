4a6cbb0a1d34796dddc485ba8308f78f9f9b7945
Marcel Goh <marcel.goh@mail.mcgill.ca>
2019-10-25 00:31:38 -0400

    Solved virus

--- assignments/a3/virus.c
+++ assignments/a3/virus.c
@@ -3,27 +3,40 @@
 #include <string.h>
 
 int main() {
-    char *before = malloc(10001*sizeof(char));
-    char *after = malloc(10001*sizeof(char));
+    char *before = malloc(100001*sizeof(char));
+    char *after = malloc(100001*sizeof(char));
     scanf("%s", before);
     scanf("%s", after);
     int b_len = strlen(before);
     int a_len = strlen(after);
 
-    if (strcmp(before, after) == 0) {
-        printf("%d\n", 0);
-    } else {
-        int first_diff = 0;
-        while (before[first_diff] == after[first_diff]) {
-            ++first_diff;
-        }
-        int last_diff = 0;
-        while (before[b_len - last_diff - 1] == after[a_len - last_diff - 1]) {
-            ++last_diff;
-        }
+    int min_len = (b_len < a_len) ? b_len : a_len;
 
-        printf("%d\n", a_len - first_diff - last_diff);
+    int first_diff = 0;
+    while (first_diff < min_len && before[first_diff] == after[first_diff]) {
+        ++first_diff;
     }
+    int last_diff = 1;
+    while (last_diff < min_len &&
+           before[b_len - last_diff] == after[a_len - last_diff]) {
+        ++last_diff;
+    }
+
+    /* number of letters that are the same in both */
+    int num_same = first_diff + last_diff - 1;
+    /* num_same cannot exceed length of first string */
+    if (num_same > b_len) {
+        num_same = b_len;
+    }
+
+    /* difference is second length - num_same; if this value
+     * is negative, just set to 0
+     */
+    int difference = (a_len - num_same >= 0) ? a_len - num_same : 0;
+    printf("%d\n", difference);
+
+    free(before);
+    free(after);
 
     return 0;
 }
